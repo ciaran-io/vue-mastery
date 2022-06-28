76df559f201c2aa8f420085320d9f98f4facf04c
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-06-28 18:53:11 +0100

    refactor: chnage eslint rules
    1. Add stricter rules set for vue
    2. Remove multiword for home dir

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,29 @@
 module.exports = {
-	root: true,
-	env: {
-		es2021,
-	},
-	extends: [
-		// add more generic rulesets here, such as:
-		// 'eslint:recommended',
-		'eslint:recommended',
-		'plugin:vue/vue3-recommended',
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
 
-		// 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
-		'prettier',
-	],
-	rules: {
-		// override/add rules settings here, such as:
-		// 'vue/no-unused-vars': 'error'
-	},
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
+    // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+    'prettier',
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: ['Home'],
+      },
+    ],
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
 };
