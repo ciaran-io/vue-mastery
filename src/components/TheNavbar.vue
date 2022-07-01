96cc3b0c25971b7850afb49e63b3378e8f1cddb4
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-07-01 10:28:30 +0100

    feat: add navbar

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <header class="bg-orange-200 py-3 container mx-auto mt-4 px-4 rounded-md">
+    <nav class="">
+      <router-link :to="{ name: 'Home' }">
+        <img src="@/assets/images/svg/logo.svg" alt="" />
+      </router-link>
+    </nav>
+  </header>
+</template>
