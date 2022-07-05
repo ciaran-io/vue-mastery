4de3e3aa21f9493896ebe362fa70017222d2d08e
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-07-05 15:11:35 +0100

    refactor: add route for thread createtion

--- src/pages/Forum.vue
+++ src/pages/Forum.vue
@@ -22,7 +22,7 @@ const threads = store.getThreadsById(props.id);
       <h2 class="text-xl text-gray-500">{{ forum.description }}</h2>
 
       <router-link
-        to="#"
+        :to="{ name: 'ThreadCreate', params: { forumId: forum.id}}"
         class="rounded-full bg-orange-500 py-2 px-4 text-sm text-white"
       >
         Start a thread
