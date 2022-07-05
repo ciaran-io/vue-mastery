1b1562dd722f827072fc308232815b175f1f88c9
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-07-05 15:32:57 +0100

    feat: update auth user props and refactor actions
    1. authUser object now returns users posts and threads.
    
    2. Add findById helper function to maintain code  & keep  DRY.
    
    3. Refactor creatPost threads & add creatThread,  updateThread action.
    
    4. Add helper function makeAppendChildParent to keep code DRY.

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,24 +1,49 @@
-import { defineStore } from 'pinia';
+import { defineStore, acceptHMRUpdate } from 'pinia';
 import sourceData from '@/data/data';
+import { findById, updateAndInsert } from '@/helpers/index';
 export const useStore = defineStore('main', {
   state: () => ({
     ...sourceData,
-    authId: '7uVPJS9GHoftN58Z2MXCYDqmNAh2',
+    authId: 'ALXhxjwgY9PinwNGHpfai6OWyDu2',
   }),
 
   getters: {
     // Return user matcing authId
-    authUser: (state) => state.users.find((user) => user.id === state.authId),
+    authUser: (state) => {
+      const user = findById(state.users, state.authId);
+
+      if (!user) return null;
+
+      return {
+        ...user,
+        // authuser.posts
+        // authuser.getPosts()
+        get posts() {
+          return state.posts.filter((post) => post.userId == user.id);
+        },
+        // authuser.postsCount
+        get postCount() {
+          return this.posts.length;
+        },
+        // authuser.threads
+        get threads() {
+          return state.threads.filter((post) => post.userId == user.id);
+        },
+        // authuser.threadsCount
+        get threadsCount() {
+          return this.threads.length;
+        },
+      };
+    },
 
     // return category matching prop.id value
     getCategoryById: (state) => {
-      return (propId) =>
-        state.categories.find((category) => category.id === propId);
+      return (propId) => findById(state.categories, propId);
     },
 
     // return forum matching prop.id value
     getForumById: (state) => {
-      return (propId) => state.forums.find((forum) => forum.id === propId);
+      return (propId) => findById(state.forums, propId);
     },
 
     // return threads matching prop.id value
@@ -29,7 +54,7 @@ export const useStore = defineStore('main', {
 
     // Return a thread matching prop.id value
     getThreadById: (state) => {
-      return (propId) => state.threads.find((thread) => thread.id === propId);
+      return (propId) => findById(state.threads, propId);
     },
 
     // return posts threadId matching prop.id value
@@ -44,7 +69,7 @@ export const useStore = defineStore('main', {
     },
 
     getUserById: (state) => {
-      return (userId) => state.users.find((user) => user.id === userId);
+      return (userId) => findById(state.users, userId);
     },
 
     // return all categories
@@ -55,15 +80,108 @@ export const useStore = defineStore('main', {
   },
 
   actions: {
-    createPost(post, threadId) {
+    createPost(post) {
       // create random thread id
       post.id = 'gggg' + Math.random();
-      // push new post to post array
-      this.posts.push(post);
-      // find thread matching the props.id
-      const thread = this.threads.find((thread) => thread.id === threadId);
-      // add new id to thread posts
-      thread.posts.push(post.id);
+      // create userId fro post
+      post.userId = this.authId;
+      // create published date  for post
+      (post.publishedAt = Math.floor(Date.now() / 1000)),
+        // push new post to post array
+        this.setPost({ post });
+      this.appendPostToThread({ parentId: post.threadId, childId: post.id });
+    },
+
+    async createThread({ title, text, forumId }) {
+      try {
+        const id = 'gggg' + Math.random();
+        // create userId for post
+        const userId = this.authId;
+        // create published date  for post
+        const publishedAt = Math.floor(Date.now() / 1000);
+        const thread = {
+          forumId,
+          title,
+          publishedAt,
+          userId,
+          id,
+        };
+
+        this.setThread({ thread });
+        this.appendThreadToForum({ parentId: forumId, childId: id });
+        this.appendThreadToUser({ parentId: userId, childId: id });
+        // create a new post and append text && threadId
+        this.createPost({ text, threadId: id });
+
+        // return thread for route navigation
+        return findById(this.threads, id);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    async updateThread({ title, text, id }) {
+      try {
+        const thread = findById(this.threads, id);
+        const post = findById(this.posts, thread.posts[0]);
+        console.log(thread);
+        const newThread = { ...thread, title };
+        const newPosts = { ...post, text };
+
+        this.setThread({ thread: newThread });
+        this.setPost({ post: newPosts });
+
+        return newThread;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    appendPostToThread: makeAppendChildParentAction({
+      parent: 'threads',
+      child: 'posts',
+    }),
+
+    appendThreadToForum: makeAppendChildParentAction({
+      parent: 'forums',
+      child: 'threads',
+    }),
+
+    appendThreadToUser: makeAppendChildParentAction({
+      parent: 'users',
+      child: 'threads',
+    }),
+
+    // find stores users
+    setUser(activeUser) {
+      const userIndex = this.users.findIndex(
+        (user) => user.id == activeUser.id
+      );
+      this.users[userIndex] = activeUser;
+    },
+
+    // find stores posts matching params and update posts
+    setPost({ post }) {
+      updateAndInsert(this.posts, post);
+    },
+    // find stores threads matching params and update posts
+    setThread({ thread }) {
+      updateAndInsert(this.threads, thread);
     },
   },
 });
+
+function makeAppendChildParentAction({ parent, child }) {
+  return ({ parentId, childId }) => {
+    // NOTE: useStore()[parent] is the same as useStore().parent cant use '.' after variable
+    const resource = findById(useStore()[parent], parentId);
+
+    resource[child] = resource[child] || [];
+    resource[child].push(childId);
+  };
+}
+
+// Add hot module replacement
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useStore, import.meta.hot));
+}
