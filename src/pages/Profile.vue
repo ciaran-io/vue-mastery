ac13db17b32eb9cf400c04758bf88e4fa58b6732
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-07-05 15:15:23 +0100

    refactor: move profile edit & post list to comp.
    1. Editing  profile data is handled in user-profile-card-editor comp.
    
    2. Users posts list is moved to post list component

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,68 +1,46 @@
 <script setup>
 import { useStore } from '@/stores/index';
-import { computed } from 'vue';
+import { computed } from 'vue'
+
+defineProps({
+  edit: {
+    type: Boolean,
+    required: false,
+    default: false,
+  }
+})
+
 const store = useStore();
-
-const user = store.authUser;
-
-const userPosts = computed(() =>
-  store.posts.filter((post) => post.userId == user.id)
-);
-
-const userPostCount = computed(() => userPosts.value.length);
-
-const userThreads = computed(() =>
-  store.threads.filter((thread) => thread.userId == user.id)
-);
-
-const userThreadsCount = computed(() => userThreads.value.length);
+const user =  computed(() => store.authUser)
 
 </script>
 
 <template>
   <div>
     <section class="space-y-4 text-right">
-      <h1 class="text-xl text-orange-400">{{ user.name }} recent activity</h1>
+      <h1 class="text-xl text-orange-400">
+        {{ user.name }} recent activity
+      </h1>
       <p>See only started threads</p>
     </section>
     <hr />
 
     <section
       aria-label="user information"
-      class="mt-8 grid lg:grid-cols-[1fr,2fr] md:gap-x-8"
+      class="mt-8 grid gap-y-12 md:gap-x-8 md:gap-y-0 lg:grid-cols-[1fr,2fr]"
     >
+      <!-- user information -->
       <div>
-        <figure>
-          <img
-            :src="user.avatar"
-            alt=""
-            width="200"
-            height="200"
-            class="avatar-profile"
-          />
-
-          <div class="mt-8 space-y-4 rounded-md p-4 shadow-md">
-            <div>Username {{ user.username }}</div>
-
-            <p>Bio {{ user.bio || 'No bio yet' }}</p>
-
-            <div class="flex gap-x-4">
-              <div>Posts {{ userPostCount || 'No posts yet' }}</div>
-              <div>Threads {{ userThreadsCount || 'No threads yet' }}</div>
-
-            </div>
-          </div>
-        </figure>
-
-        <button class="mt-4 rounded-full bg-orange-500 px-4 py-2 text-white ml-auto w-max block">
-          Edit Profile
-        </button>
+        <user-profile-card v-if='!edit' :user="user"> </user-profile-card>
+        <user-profile-card-editor v-else
+          :user="user"
+        ></user-profile-card-editor>
       </div>
 
+      <!-- user posts -->
       <section aria-label="users posts" class="space-y-12">
-        <post-list :posts="userPosts"></post-list>
+        <post-list :posts="user.posts"></post-list>
       </section>
-
     </section>
   </div>
 </template>
