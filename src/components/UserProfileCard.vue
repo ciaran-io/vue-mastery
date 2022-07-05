76127302706dd3793000b126800af06278ecbbec
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-07-05 15:08:02 +0100

    feat: add user profile card
    1. Displays user information

---
+++ src/components/UserProfileCard.vue
@@ -0,0 +1,54 @@
+<script setup>
+defineProps({
+  user: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <figure>
+      <img
+        :src="user.avatar"
+        alt=""
+        width="200"
+        height="200"
+        class="avatar-profile"
+      />
+
+      <div class="mt-8 space-y-4 rounded-md p-4 shadow-md">
+        <div class="flex justify-between">
+          <div class="capitalize">
+            <span class="font-semibold">Username</span>
+            {{ user.username }}
+          </div>
+
+          <div class="flex gap-x-4">
+            <div>
+              <span class="font-semibold">Posts</span>
+              {{ user.postCount || 'No posts yet' }}
+            </div>
+
+            <div>
+              <span class="font-semibold">Threads</span>
+              {{ user.threadsCount || 'No threads yet' }}
+            </div>
+          </div>
+        </div>
+        <!-- user bio -->
+        <p>
+          <span class="font-semibold">Bio</span>
+          {{ user.bio || 'No bio yet' }}
+        </p>
+      </div>
+    </figure>
+
+    <router-link :to="{ name: 'ProfileEdit'}"
+      class="mt-4 ml-auto block w-max rounded-full bg-orange-500 px-4 py-2 text-white"
+    >
+      Edit Profile
+    </router-link>
+  </div>
+</template>
