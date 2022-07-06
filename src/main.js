1492e55226f34e5d0dae3700f9e416330c927f3a
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-07-06 14:30:14 +0100

    feat: add firebase setup and intallation

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from '@/router';
 import '@/assets/css/main.css';
+import firebaseConfig from '@/config/firebase';
+import { initializeApp } from 'firebase/app';
 
 const pinia = createPinia();
 const forumApp = createApp(App);
+
+initializeApp(firebaseConfig);
 forumApp.use(router);
 forumApp.use(pinia);
 
