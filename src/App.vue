47b4a58e96d0968f6f2c1c720ab494692f79de3c
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-07-25 10:24:53 +0100

    refactor: auth user is now retrieved from store

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <script setup>
-import { ref,  } from 'vue';
-import { useStore } from './stores';
-
 const store = useStore();
-const authUser = ref({});
-
-(async function getAuthUser() {
-  authUser.value = await store.fetchAuthUser();
-})();
-
-
+store.fetchAuthUser();
 </script>
 
 <template>
-  <the-navbar :auth-user="authUser"></the-navbar>
+  <the-navbar />
   <div class="container mx-auto mt-16 px-4">
     <router-view />
   </div>
